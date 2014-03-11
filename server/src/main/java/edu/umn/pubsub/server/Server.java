3bc210d40fb8b818fa7df965ed8a46b8d2cf5cb5
prashantc29 <prash.c.29@gmail.com>
2014-03-11 17:34:45 -0500

    #1 server commit

---
+++ server/src/main/java/edu/umn/pubsub/server/Server.java
@@ -0,0 +1,4 @@
+package edu.umn.pubsub.server;
+
+public class Server {
+}
---
+++ server/src/test/java/edu/umn/pubsub/server/TestServer.java
@@ -0,0 +1,9 @@
+package edu.umn.pubsub.server;
+
+public class TestServer {
+
+	public static void main(String[] args) {
+
+	}
+
+}
