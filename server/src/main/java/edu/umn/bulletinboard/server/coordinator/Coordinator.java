57d5124e97e520fe775bdf4459fc3966a0cde721
Abhijeet Gaikwad <abygaikwad17@gmail.com>
2014-03-31 00:16:24 -0500

    #12 Coordinator implementaiton, few compile issues.

--- server/src/main/java/edu/umn/bulletinboard/server/coordinator/Coordinator.java
+++ server/src/main/java/edu/umn/bulletinboard/server/coordinator/Coordinator.java
@@ -1,16 +1,27 @@
 package edu.umn.bulletinboard.server.coordinator;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+import edu.umn.bulletinboard.common.constants.RMIConstants;
 import edu.umn.bulletinboard.common.content.Article;
 import edu.umn.bulletinboard.common.content.RegisterRet;
 import edu.umn.bulletinboard.common.exception.IllegalIPException;
 import edu.umn.bulletinboard.common.locks.ServerLock;
+import edu.umn.bulletinboard.common.rmi.BulletinBoardService;
 import edu.umn.bulletinboard.common.server.ServerInfo;
 import edu.umn.bulletinboard.common.util.ConsistencyType;
+import edu.umn.bulletinboard.common.util.IndentArticles;
 import edu.umn.bulletinboard.common.util.LogUtil;
+import edu.umn.bulletinboard.server.BulletinBoardServiceImpl;
+import edu.umn.bulletinboard.server.Server;
+import edu.umn.bulletinboard.server.ServerConfig;
+import edu.umn.bulletinboard.server.exceptions.InvalidArticleException;
+import edu.umn.bulletinboard.server.storage.MemStore;
 
 /**
  * @author abhijeet
@@ -27,8 +38,19 @@ public class Coordinator {
     //This is an assumption that number of articles cannot be more than
     //Max value of integer in Java
 	int counter, serverIdCounter;
-    int readQ, writeQ;
-    List<ServerInfo> servers = new ArrayList<ServerInfo>();
+    Map<Integer, ServerInfo> servers = new HashMap<Integer, ServerInfo>();
+
+    private static Coordinator instance = null;
+
+    private Coordinator() {}
+
+    public synchronized static Coordinator getInstance() {
+        if (null == instance) {
+            instance = new Coordinator();
+        }
+
+        return instance;
+    }
 
     /**
      * Read call from server. Should be mostly used in Quorum consistency.
@@ -37,16 +59,61 @@ public class Coordinator {
      * @return
      * @throws RemoteException
      */
-	public List<Article> readFromCoordinatingServer(ConsistencyType type)
-			throws RemoteException {
+	public synchronized List<Article> readFromCoordinatingServer(ConsistencyType type)
+            throws RemoteException, MalformedURLException, NotBoundException {
 
         //for quorum consistency only
+        //TODO: pick up consistency level from properties file
+        if (type != ConsistencyType.QUORUM) {
+            throw new RemoteException("Not a quorum consistency");
+        }
 
-        //get all the data from any random readQ servers.
         //see which is the max value, get all the values from that server and return them
-
-		return null;
+        BulletinBoardService client = getClient(servers.get(getLatestUpdatedServerId()));
+        return client.readFromServer();
 	}
+
+    private int getLatestUpdatedServerId() throws RemoteException, NotBoundException
+            , MalformedURLException {
+        Random random = new Random();
+
+        int max = 0, maxServer = 0;
+        for (int i = 0; i < getNR(); ++i) {
+            int servId = random.nextInt(serverIdCounter + 2); //exclusive
+
+            if (servId == serverIdCounter + 1) {
+                servId = 99;
+            }
+
+            if (0 == servId || null == servers.get(servId)) {
+                --i;
+                continue;
+            }
+
+            BulletinBoardService client = getClient(servers.get(servId));
+            if (client.getLatestArticleId() > max) {
+                max = client.getLatestArticleId();
+                maxServer = servId;
+            }
+        }
+
+        return maxServer;
+    }
+
+    private BulletinBoardService getClient(ServerInfo sInfo) throws RemoteException
+            , NotBoundException, MalformedURLException {
+
+        BulletinBoardService client = null;
+        if (99 == Server.getServerId()) {
+            client = BulletinBoardServiceImpl.getInstance();
+        } else {
+            client = (BulletinBoardService) Naming.lookup("rmi://"
+                    + sInfo.getIp() + ":" + sInfo.getPort()
+                    + "/" + RMIConstants.BB_SERVICE);
+        }
+
+        return client;
+    }
 
     /**
      * Choose call from server.
@@ -56,13 +123,34 @@ public class Coordinator {
      * @return
      * @throws RemoteException
      */
-	public Article chooseFromCoordinatingServer(int id, ConsistencyType type)
-			throws RemoteException {
+	public synchronized Article chooseFromCoordinatingServer(int id, ConsistencyType type)
+            throws RemoteException, MalformedURLException, NotBoundException {
 
-        //choose for server? means what?
+        //for quorum consistency only
+        //TODO: pick up consistency level from properties file
+        if (type != ConsistencyType.QUORUM) {
+            throw new RemoteException("Not a quorum consistency");
+        }
 
-		return null;
+        int maxServer = getLatestUpdatedServerId();
+
+        //see which is the max value, get all the values from that server and return them
+        BulletinBoardService client = getClient(servers.get(getLatestUpdatedServerId()));
+        return client.readFromServer(id);
 	}
+
+    private void syncAll(int id, Article article) throws RemoteException, NotBoundException
+            , MalformedURLException {
+
+        for (int i : servers.keySet()) {
+            BulletinBoardService client = getClient(servers.get(i));
+            if (-1 == id) {
+                client.writeToServer(article);
+            } else {
+                client.replyToServer(id, article);
+            }
+        }
+    }
 
     /**
      * Write call from Server.
@@ -72,24 +160,66 @@ public class Coordinator {
      * @return
      * @throws RemoteException
      */
-    public int writeToCoordinatingServer(Article articleText, ConsistencyType type)
-            throws RemoteException {
+    public synchronized int writeToCoordinatingServer(Article articleText, ConsistencyType type)
+            throws RemoteException, MalformedURLException, NotBoundException, InvalidArticleException {
+        return writeReply(-1, articleText, type);
+    }
 
-        return 0;
+    private int writeReply(int id, Article articleText, ConsistencyType type)
+            throws RemoteException, InvalidArticleException, MalformedURLException
+            , NotBoundException {
+        if (! (type == ConsistencyType.QUORUM || type == ConsistencyType.SEQUENTIAL)) {
+            throw new RemoteException("Not a quorum/sequential consistency");
+        }
+
+        articleText.setId(++counter);
+
+        if (type == ConsistencyType.SEQUENTIAL) {
+            syncAll(id, articleText);
+            return counter;
+        }
+
+        //quorum consistency
+        //add the article
+
+        Random random = new Random();
+        for (int i = 0; i < getNW(); ++i) {
+            int servId = random.nextInt(serverIdCounter + 2); //exclusive
+
+            if (servId == serverIdCounter + 1) {
+                servId = 99;
+            }
+
+            if (0 == servId || null == servers.get(servId)) {
+                --i;
+                continue;
+            }
+
+            BulletinBoardService client = getClient(servers.get(servId));
+            if (-1 == id) {
+                client.writeToServer(articleText);
+            } else {
+                client.replyToServer(id, articleText);
+            }
+        }
+
+        return counter;
+
     }
 
     /**
      * Reply call from server.
      *
      * @param articleId
+     * @param article
      * @param type
      * @return
      * @throws RemoteException
      */
-    public int replyToCoordinatingServer(int articleId, ConsistencyType type)
-            throws RemoteException {
-
-        return 0;
+    public synchronized int replyToCoordinatingServer(int articleId, Article article
+            , ConsistencyType type) throws RemoteException, InvalidArticleException
+            , NotBoundException, MalformedURLException {
+        return writeReply(articleId, article, type);
     }
 
     /**
@@ -98,7 +228,7 @@ public class Coordinator {
      * @return article id
      * @throws RemoteException
      */
-	public int getNextArticleID() throws RemoteException {
+	public synchronized int getNextArticleID() throws RemoteException {
 
         // this should be synchronized as a lot of Servers will simultaneously
         // call this method.
@@ -114,15 +244,16 @@ public class Coordinator {
      * @return server id
      * @throws RemoteException
      */
-    public RegisterRet register(String ip, int port) throws RemoteException {
+    public synchronized RegisterRet register(String ip, int port) throws RemoteException {
 
         RegisterRet ret = null;
 
         synchronized (ServerLock.register) {
             ++serverIdCounter;
             try {
-                ret = new RegisterRet(serverIdCounter, servers);
-                servers.add(serverIdCounter, new ServerInfo(ip, port));
+                servers.put(serverIdCounter, new ServerInfo(ip, port));
+                ret = new RegisterRet(serverIdCounter, new ArrayList<ServerInfo>(
+                        servers.values()));
             } catch (IllegalIPException e) {
                 throw new RemoteException(e.getMessage());
             }
@@ -131,11 +262,14 @@ public class Coordinator {
         return ret;
     }
 
-    public void sync(List<Article> articles) throws RemoteException {
-
+    private int getNR() {
+        return ServerConfig.getNR();
     }
-    
-    public List<ServerInfo> getRegisteredServers() throws RemoteException {
-		return null;
-	}
+
+    private int getNW() {
+        if (-1 == ServerConfig.getNW()) {
+            return servers.size();
+        }
+        return ServerConfig.getNW();
+    }
 }
